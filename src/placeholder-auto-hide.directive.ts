7de746ae37de0880e4905e634fc810dcd6f02935
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-08-08 17:33:02 +0100

    add auto hide directive logic

--- src/placeholder-auto-hide.directive.ts
+++ src/placeholder-auto-hide.directive.ts
@@ -1,11 +1,25 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef,Input,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[placeholder-auto-hide]'
 })
-export class PlaceholderAutoHideDirective {
+export class AutoHideDirective  {
+ 
+  @Input() placeholder: string;
 
   constructor(private el: ElementRef) {
+  
+  }
+
+  @HostListener('focus')
+  onFocus(){
+    this.el.nativeElement.setAttribute('placeholder','');
+  }
+
+   @HostListener('blur')
+   onBlur(){
+    this.el.nativeElement.setAttribute('placeholder',this.placeholder);
+
   }
 
 }
